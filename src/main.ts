dd180dc2168c10d2dba192018d35a4d86dfbaa82
yanivsela6 <selayaniv6@gmail.com>
2022-03-13 16:12:15 +0200

    123

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+1231
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
