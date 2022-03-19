c97b55aa243ea5db155d50e78d1194f6ae947e98
selashilo7 <selashilo7@gmail.com>
2022-03-19 23:34:40 +0200

    first map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularCesiumModule, AngularCesiumWidgetsModule } from 'angular-cesium';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,8 +13,11 @@ import { MapComponent } from './components/map/map.component';
     MapComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularCesiumModule.forRoot(),
+    AngularCesiumWidgetsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,15 +1,90 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import {
+  AcMapComponent,
+  AcNotification,
+  ViewerConfiguration,
+  ActionType,
+  CameraService,
+} from 'angular-cesium';
+import { Observable, of } from 'rxjs';
+const randomLocation = require('random-location');
+declare var Cesium: any;
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
+  providers: [ViewerConfiguration],
 })
-export class MapComponent implements OnInit {
-
-  constructor() { }
-
+export class MapComponent implements OnInit, AfterViewInit {
+  constructor(private viewerConf: ViewerConfiguration) {
+    viewerConf.viewerOptions = {
+      selectionIndicator: false,
+      timeline: false,
+      infoBox: false,
+      fullscreenButton: false,
+      baseLayerPicker: false,
+      animation: false,
+      homeButton: false,
+      geocoder: false,
+      navigationHelpButton: false,
+      navigationInstructionsInitiallyVisible: false,
+      useDefaultRenderLoop: true,
+    };
+  }
+  @ViewChild('map') map!: AcMapComponent;
+  entities$!: Observable<AcNotification>;
+  private camera!: CameraService;
+  Cesium = Cesium;
+  ngAfterViewInit(): void {
+    this.camera = this.map.getCameraService();
+  }
   ngOnInit(): void {
+  }
+  goHome(): void {
+    navigator.geolocation.getCurrentPosition(
+      (data) => {
+        const { latitude, longitude } = data.coords;
+        const position = Cesium.Cartesian3.fromDegrees(longitude, latitude);
+        const entity = {
+          id: 'my-home',
+          position,
+        };
+        this.entities$ = of({
+          id: entity.id,
+          actionType: ActionType.ADD_UPDATE,
+          entity,
+        });
+        this.zoomToLocation(position, 1000);
+      },
+      (err) => {
+        console.log(err);
+      },
+      { enableHighAccuracy: true, timeout: 10000 }
+    );
+  }
+  goRandom(): void {
+    const randomStart = {
+      latitude: 37.7768006 * Math.random(),
+      longitude: -122.4187928 * Math.random(),
+    };
+    const radius = 5000000000 * Math.random(); // meters
+    const { latitude, longitude } = randomLocation.randomCirclePoint(
+      randomStart,
+      radius
+    );
+
+    this.zoomToLocation(
+      Cesium.Cartesian3.fromDegrees(longitude, latitude),
+      100000
+    );
+  }
+  private zoomToLocation(position: any, zoom: number): void {
+    this.camera.cameraFlyTo({
+      destination: position,
+      complete: () => {
+        this.camera.zoomOut(zoom);
+      },
+    });
   }
 
 }
